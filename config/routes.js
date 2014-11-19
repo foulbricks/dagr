13c15281c2f0549645118463a6da22cedbc93a86
Spanishdobloon <spanishdobloon@administrators-imac81.local>
2014-11-19 15:42:12 -0500

    Moving routes/views/models to app folder

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,12 @@
 module.exports = function(app, dir){
-	var routes = 	require(dir + '/routes/index');
-	var users = 	require(dir + '/routes/users');
-	var errors = 	require(dir + "/routes/errors");
+	var routes = 	require(dir + '/app/routes/index');
+	var users = 	require(dir + '/app/routes/users');
+	var errors = 	require(dir + "/app/routes/errors");
 	
 	//app.use('/', routes);
 	app.use('/users', users);
 	app.get("*", function(req, res){
-		res.sendfile('./public/angular/views/index.html');
+		res.sendfile(dir + '/public/angular/views/index.html');
 	});
 	app.use(errors);
 }
