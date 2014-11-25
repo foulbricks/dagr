45ca4a9dc1213dc5f12269323e149a8c9a18fd3c
foulbricks <foulbricks@gmail.com>
2014-11-25 18:52:24 -0500

    Playing with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,26 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        clean: ["dist", '.tmp'],
+        clean: ["public/dist", '.tmp'],
 
         copy: {
             main: {
                 expand: true,
-                cwd: '',
-                src:['app/**','scripts/web-server.js','!app/lib/**','!app/modules/**/*.js','!app/js/**','!app/css/**'],
-                dest: 'dist/'
+                cwd: 'app/angular',
+                src:['views/*.html', '**/views/*.html'],
+                dest: 'public/dist/'
             }
         },
 
         useminPrepare: {
-            html: 'app/index.html'
+            html: 'app/angular/views/index.html',
+			options: {
+		    	dest: 'public'
+		    }
         },
 
         usemin: {
-            html: ['dist/app/index.html']
+            html: ['public/dist/views/index.html']
         },
 
         uglify: {
@@ -37,26 +40,26 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-usemin');
 
-    grunt.registerTask('adjustBuiltLinks', 'Performing Final Cleanups', function() {
-
-        var indexDist='dist/app/index.html';
-
-        grunt.log.writeln('Performing Final Cleanups');
-
-        var indexContent=grunt.file.read(indexDist);
-
-        indexContent=indexContent.replace('<link rel="stylesheet" href="app/built/app.min.css"/>','<link rel="stylesheet" href="built/app.min.css"/>');
-
-        indexContent=indexContent.replace('<script src="app/built/app.min.js"></script>','<script src="built/app.min.js"></script>');
-
-        grunt.file.write(indexDist, indexContent);
-
-    });
+    // grunt.registerTask('adjustBuiltLinks', 'Performing Final Cleanups', function() {
+    // 
+    //     var indexDist='dist/app/index.html';
+    // 
+    //     grunt.log.writeln('Performing Final Cleanups');
+    // 
+    //     var indexContent=grunt.file.read(indexDist);
+    // 
+    //     indexContent=indexContent.replace('<link rel="stylesheet" href="app/built/app.min.css"/>','<link rel="stylesheet" href="built/app.min.css"/>');
+    // 
+    //     indexContent=indexContent.replace('<script src="app/built/app.min.js"></script>','<script src="built/app.min.js"></script>');
+    // 
+    //     grunt.file.write(indexDist, indexContent);
+    // 
+    // });
 
 
     // Tell Grunt what to do when we type "grunt" into the terminal
     grunt.registerTask('default', [
-       'clean', 'copy', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin','adjustBuiltLinks'
+       'clean', 'copy', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin'// ,'adjustBuiltLinks'
     ]);
 
 };
--- public/angular/modules/workspaces/workspaceServices.js
+++
@@ -1,1 +0,0 @@
-angular.module("Dagr.admin.services", [])
